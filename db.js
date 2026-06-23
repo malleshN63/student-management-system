377d5c2756161eb9e2576bea77a440d933140c2c
MalleshK327 <mallesh.n@quintype.com>
2026-06-23 16:14:34 +0530

    Add MySQL service for GitHub Actions

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "root",
     database: "student_management"
 });
 
